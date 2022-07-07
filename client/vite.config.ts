3715b650231ab3067fd4c91d18c23aec9de3a10f
wuls <linsheng.wu@beantechs.com>
2022-07-07 20:37:04 +0800

    init project

---
+++ client/vite.config.ts
@@ -0,0 +1,45 @@
+import { join } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Icons from 'unplugin-icons/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Unocss from 'unocss/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/__inspect/',
+  resolve: {
+    alias: {
+      '~/': __dirname,
+    },
+  },
+  plugins: [
+    vue(),
+    Pages({
+      pagesDir: 'pages',
+    }),
+    Icons(),
+    Components({
+      dirs: [
+        'components',
+      ],
+      dts: join(__dirname, 'components.d.ts'),
+      resolvers: [
+        IconsResolver({
+          componentPrefix: '',
+        }),
+      ],
+    }),
+    Unocss(),
+    AutoImport({
+      dts: join(__dirname, 'auto-imports.d.ts'),
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+    }),
+  ],
+})
